56d8fe50372e3f65c0414e8673a08b12a47e8dc0
Anna Moore <annathekarategirl@gmail.com>
2026-04-27 13:18:11 -0400

    wowwie yowwiee

---
+++ script.js
@@ -0,0 +1,45 @@
+/*
+Learning sources
+https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+https://www.geeksforgeeks.org/javascript/javascript-coordinates-of-mouse/
+https://www.w3schools.com/html/html5_canvas.asp#:~:text=Learn%20how%20to%20draw%20graphics%20on%20a%20web%20page
+*/
+/*
+some testing
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "green";
+console.log("x: ",Event.clientX)
+console.log("y",event.clientY)
+ctx.fillRect(100, 100, 10, 10);
+
+
+var e = window.event;
+
+    var posX = e.clientX;
+    var posY = e.clientY;
+
+    console.log(posX,posY) */
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "black";
+
+ctx.fillRect(100, 100, 10, 10);
+
+//event listener for mouse down and funciton draw adds event listener for mouse move then mouse up removes event listener
+function drawing() {
+    console.log("works")
+}
+
+
+function mouseDownDraw(){
+    console.log("yay")
+    canvas.addEventListener("mousemove",drawing)
+
+    canvas.addEventListener("mouseup",function(){canvas.removeEventListener("mousemove")})
+}
+
+canvas.addEventListener("mousedown",mouseDownDraw)
