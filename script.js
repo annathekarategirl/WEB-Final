d2d17910a2d77d1b8d2215822457bcb8e6b383d5
Anna Moore <annathekarategirl@gmail.com>
2026-04-27 14:10:09 -0400

    TS IS PEAKJ

--- script.js
+++ script.js
@@ -24,24 +24,64 @@ var e = window.event;
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const sizeInput=document.getElementById("sizeInput")
+let isDrawing=false
 
 ctx.fillStyle = "black";
 
 //event listener for mouse down and funciton draw adds event listener for mouse move then mouse up removes event listener
-function drawing() {
-    console.log("works")
-    let x = event.clientX;
+
+
+
+class Tool{
+    //Abstract not really tho
+    constructor(brushSize,color,htmlelement){
+        this.array=[]
+        this.brushSize=brushSize
+        this.color=color
+        this.htmlelement=htmlelement
+
+    }
+    
+    changeBrushSize() {
+        this.brushSize=Number(sizeInput.value)
+        console.log(this.brushSize)
+    }
+
+    drawing(event) {
+        if (isDrawing){
+        console.log("works")
+        let x = event.clientX;
         let y = event.clientY;
-    ctx.fillRect(x, y, 10, 10);
+        ctx.fillRect(x, y, this.brushSize, this.brushSize);
+        }
+    }
+
+    mouseDownDraw(ev){
+        console.log("yay")
+        // this.drawing('test')
+        isDrawing=true
+        
+    }
+
+}
+//to fill in gaps, draw line between them
+//global boolean of is drawing insteead of removing event listeners
+let ultimateTool=new Tool()
+
+class Brush extends Tool{
+    constructor(brushSize,color){
+        super(brushSize,color)
+    }
 }
 
+let defaultBrush=new Brush(10,"black")
+defaultBrush.changeBrushSize()
+let currentBrush=defaultBrush
 
-function mouseDownDraw(){
-    console.log("yay")
-    canvas.addEventListener("mousemove",drawing)
-
-    canvas.addEventListener("mouseup",function(){canvas.removeEventListener("mousemove",drawing)})
-    canvas.addEventListener("mouseleave",function(){canvas.removeEventListener("mousemove",drawing)})
-}
-
-canvas.addEventListener("mousedown",mouseDownDraw)
+canvas.addEventListener("mousemove",(event) => {currentBrush.drawing(event)})
+canvas.addEventListener("mousedown",function(){isDrawing=true})
+canvas.addEventListener("mouseup",function(){isDrawing=false})
+canvas.addEventListener("mouseleave",function(){isDrawing=false})
+//canvas.addEventListener("mousedown",(ev) => {currentBrush.mouseDownDraw(ev)})
+sizeInput.addEventListener("change",(ev) => {currentBrush.changeBrushSize(ev)})
